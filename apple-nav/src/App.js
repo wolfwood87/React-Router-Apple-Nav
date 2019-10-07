720042f7589e41df744abcd71641c56b545581ba
Derek <legato_5@yahoo.com>
2019-10-06 18:25:18 -0700

    added json object for mac images and installed react-router-dom

---
+++ apple-nav/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Route path='/' component={} */}
+    </div>
+  );
+}
+
+export default App;
